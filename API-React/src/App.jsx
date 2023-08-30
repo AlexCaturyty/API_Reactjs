c530f408e2fac26cf0a2e9312ffb5727a7a44ab1
AlexCavalcantiDev <104162816+AlexCaturyty@users.noreply.github.com>
2023-08-30 23:10:21 +0000

    Add API 3

--- API-React/src/App.jsx
+++ API-React/src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  
+
 
   const [pokemonName, setPokemonName] = useState('');
   const [pokemonData, setPokemonData] = useState(null);
   const [inputCep, setInputCep] = useState('');
   const [cepData, setCepData] = useState(null);
-
-
-
+  const [starWarsCharacterName, setStarWarsCharacterName] = useState('');
+  const [starWarsCharacterData, setStarWarsCharacterData] = useState(null);
 
   const handleSearch = () => {
     if (pokemonName.trim() !== '') {
@@ -40,58 +39,80 @@ function App() {
     }
   };
 
-  const handleNumberFactSearch = () => {
-    if (numberFact.trim() !== '') {
-      fetch(`http://numbersapi.com/${numberFact}`)
-        .then(response => response.text())
-        .then(data => setNumberFactData(data))
+  const handleStarWarsSearch = () => {
+    if (starWarsCharacterName.trim() !== '') {
+      fetch(`https://swapi.dev/api/people/?search=${starWarsCharacterName}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.results && data.results.length > 0) {
+            setStarWarsCharacterData(data.results[0]);
+          } else {
+            setStarWarsCharacterData(null);
+          }
+        })
         .catch(error => {
-          console.error('Erro na busca de curiosidade numérica:', error);
-          setNumberFactData('');
+          console.error('Erro na busca do personagem de Star Wars:', error);
+          setStarWarsCharacterData(null);
         });
     }
   };
 
-  return (
-    <div>
-      <h1>Buscar Pokémon por Nome</h1>
+
+    return (
+      <div>
+        <h1>Buscar Pokémon por Nome</h1>
+        <input
+          type="text"
+          value={pokemonName}
+          onChange={e => setPokemonName(e.target.value)}
+          placeholder="Digite o nome do Pokémon"
+        />
+        <button onClick={handleSearch}>Buscar</button>
+
+        {pokemonData && (
+          <div>
+            <h2>{pokemonData.name}</h2>
+            <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+          </div>
+        )}
+
+        <h1>Consultar CEP</h1>
+        <input
+          type="text"
+          value={inputCep}
+          onChange={e => setInputCep(e.target.value)}
+          placeholder="Digite o CEP"
+        />
+        <button onClick={handleCepSearch}>Consultar</button>
+
+        {cepData && (
+          <div>
+            <p>CEP: {cepData.cep}</p>
+            <p>Rua: {cepData.logradouro}</p>
+          </div>
+        )}
+
+<h1>Buscar Personagem de Star Wars</h1>
       <input
         type="text"
-        value={pokemonName}
-        onChange={e => setPokemonName(e.target.value)}
-        placeholder="Digite o nome do Pokémon"
+        value={starWarsCharacterName}
+        onChange={e => setStarWarsCharacterName(e.target.value)}
+        placeholder="Digite o nome do personagem de Star Wars"
       />
-      <button onClick={handleSearch}>Buscar</button>
+      <button onClick={handleStarWarsSearch}>Buscar</button>
 
-      {pokemonData && (
+      {starWarsCharacterData && (
         <div>
-          <h2>{pokemonData.name}</h2>
-          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+          <h2>{starWarsCharacterData.name}</h2>
+          <p>Gênero: {starWarsCharacterData.gender}</p>
+          <p>Ano de nascimento: {starWarsCharacterData.birth_year}</p>
         </div>
       )}
-
-      <h1>Consultar CEP</h1>
-      <input
-        type="text"
-        value={inputCep}
-        onChange={e => setInputCep(e.target.value)}
-        placeholder="Digite o CEP"
-      />
-      <button onClick={handleCepSearch}>Consultar</button>
-
-      {cepData && (
-        <div>
-          <p>CEP: {cepData.cep}</p>
-          <p>Rua: {cepData.logradouro}</p>
-        </div>
-      )}
-
-
 
       </div>
-  );
+    );
 
-  
-}
 
-export default App;
+  }
+
+  export default App;
